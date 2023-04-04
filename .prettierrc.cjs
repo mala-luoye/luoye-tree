efd7e79d1919cfaca7de7c7ee9a7726d269d8175
lilian <1220795758@qq.com>
2023-04-05 00:43:24 +0800

    build: 1.0.0

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  useTabs: false, // 采用tab缩进还是空白缩进
-  tabWidth: 2, // Tab键宽度
-  printWidth: 80, // 宽度限制
-  singleQuote: false, //字符串是否使用单引号
-  semi: false, //行位是否使用分号
-  trailingComma: "none", //是否使用尾逗号
-  bracketSpacing: true // 是否保留括号中的空格 默认true
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: false,
+  semi: false,
+  trailingComma: "none",
+  bracketSpacing: true
 }
