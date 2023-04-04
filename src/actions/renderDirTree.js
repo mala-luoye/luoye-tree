72f20ca15c24ed50211ea3bce2905a7b8b7b6109
lilian <1220795758@qq.com>
2023-04-05 00:24:25 +0800

    feat: renderDirTree美化

--- src/actions/renderDirTree.js
+++ src/actions/renderDirTree.js
@@ -1,10 +1,18 @@
-const renderDirTree = (dirTree, level = 0) => {
-  const indent = "  ".repeat(level)
-  console.log(`${indent}${dirTree.value}`)
-
-  for (const child of dirTree.children) {
-    renderDirTree(child, level + 1)
+const renderDirTree = (dirTree) => {
+  console.log(dirTree.value)
+  const printNode = (node, prefix = "", isLast = true) => {
+    const newPrefix = prefix + (isLast ? "└── " : "├── ")
+    console.log(newPrefix + node.value)
+    const lastChildIndex = node.children.length - 1
+    node.children.forEach((child, index) => {
+      const isLastChild = index === lastChildIndex
+      const newChildPrefix = prefix + (isLast ? "    " : "│   ")
+      printNode(child, newChildPrefix, isLastChild)
+    })
   }
+  dirTree.children.forEach((child) => {
+    printNode(child)
+  })
 }
 
 module.exports = renderDirTree
